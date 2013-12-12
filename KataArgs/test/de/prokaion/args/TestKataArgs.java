e77361be936d4e17861036032507737bbdedc293
prokaion <daniel.mondry@idealo.de>
2013-12-12 21:05:35 +0100

    Init

---
+++ KataArgs/test/de/prokaion/args/TestKataArgs.java
@@ -0,0 +1,14 @@
+package de.prokaion.args;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestKataArgs {
+    
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    
+}
